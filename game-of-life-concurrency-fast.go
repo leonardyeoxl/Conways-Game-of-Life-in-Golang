50f9ec298d4ee64308f56ff7982a07a574f49dd8
leonardyeoxl <leonardyeoxl@gmail.com>
2021-09-06 12:33:11 +0800

    simply files

--- game-of-life-concurrency-fast.go
+++ game-of-life-concurrency-fast.go
@@ -1,29 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
-	// "time"
 	"sync"
 )
 
-const maxRows = 500
-const maxCols = 500
+var (
+	maxRows = flag.Int("max_rows", 500, "maximum rows in world")
+	maxCols = flag.Int("max_cols", 500, "maximum columns in world")
+	displayWorld = flag.Bool("display_world", false, "display world on console")
+)
 
 func rowLine() {
 	fmt.Print("\n")
-	for i := 0; i < maxCols; i++ {
+	for i := 0; i < *maxCols; i++ {
 		fmt.Print(" -----")
 	}
 	fmt.Print("\n")
 }
 
-func countLiveNeighbourCell(arr [maxRows][maxCols]int, curr_row int, curr_col int) int {
+func countLiveNeighbourCell(arr [][]int, curr_row int, curr_col int) int {
 	count := 0
 
 	for i := curr_row-1; i <= curr_row+1; i++ {
 		for j := curr_col-1; j <= curr_col+1; j++ {
-			if (i==curr_row && j==curr_col) || (i<0 || j<0) || (i>=maxRows || j>=maxCols) {
+			if (i==curr_row && j==curr_col) || (i<0 || j<0) || (i>=*maxRows || j>=*maxCols) {
 				continue
 			}
 			if arr[i][j]==1 {
@@ -35,22 +38,22 @@ func countLiveNeighbourCell(arr [maxRows][maxCols]int, curr_row int, curr_col in
 	return count
 }
 
-func nextWorldState(world01 *[maxRows][maxCols]int, world02 *[maxRows][maxCols]int) {
+func nextWorldState(world01 *[][]int, world02 *[][]int) {
 	var wg sync.WaitGroup
-	wg.Add(maxRows)
+	wg.Add(*maxRows)
 	neighbour_live_cell := 0
 	
-	for i := 0; i < maxRows; i++ {
+	for i := 0; i < *maxRows; i++ {
 		go func(i int){
-			for j := 0; j < maxCols; j++ {
+			for j := 0; j < *maxCols; j++ {
 				neighbour_live_cell = countLiveNeighbourCell(*world01, i, j)
 
 				if (*world01)[i][j]==1 && (neighbour_live_cell==2 || neighbour_live_cell==3) {
-					world02[i][j] = 1
+					(*world02)[i][j] = 1
 				} else if (*world01)[i][j]==0 && neighbour_live_cell==3 {
-					world02[i][j] = 1
+					(*world02)[i][j] = 1
 				} else {
-					world02[i][j] = 0
+					(*world02)[i][j] = 0
 				}
 			}
 			wg.Done()
@@ -60,15 +63,52 @@ func nextWorldState(world01 *[maxRows][maxCols]int, world02 *[maxRows][maxCols]i
 	wg.Wait()
 }
 
-func main() {
-	world01 := [maxRows][maxCols]int{}
-	world02 := [maxRows][maxCols]int{}
+func printWorld(world *[][]int) {
+	rowLine()
+    for i := 0; i < *maxRows; i++ {
+        fmt.Print(":")
+        for j := 0; j < *maxCols; j++ {
+            fmt.Printf("  %d  :", (*world)[i][j])
+        }
+        rowLine()
+    }
+}
 
-	for i := 0; i < maxRows; i++ {
-		for j := 0; j < maxCols; j++ {
+
+func main() {
+	flag.Parse()
+
+	world01 := make([][]int, *maxRows)
+	for i := 0; i < *maxRows; i++ {
+		world01[i] = make([]int, *maxCols)
+	}
+
+	for i := 0; i < *maxRows; i++ {
+		for j := 0; j < *maxCols; j++ {
 			world01[i][j] = rand.Intn(32767) % 2
 		}
 	}
 
+	world02 := make([][]int, *maxRows)
+	for i := 0; i < *maxRows; i++ {
+		world02[i] = make([]int, *maxCols)
+	}
+
+	for i := 0; i < *maxRows; i++ {
+		for j := 0; j < *maxCols; j++ {
+			world02[i][j] = 0
+		}
+	}
+
+	if *displayWorld {
+		printWorld(&world01)
+	}
+	
+
 	nextWorldState(&world01, &world02)
+
+	if *displayWorld {
+		fmt.Println("Next Generation:")
+		printWorld(&world02)
+	}
 }
--- game-of-life-sequence.go
+++ game-of-life-sequence.go
@@ -1,27 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 )
 
-const maxRows = 500
-const maxCols = 500
+var (
+	maxRows = flag.Int("max_rows", 500, "maximum rows in world")
+	maxCols = flag.Int("max_cols", 500, "maximum columns in world")
+	displayWorld = flag.Bool("display_world", false, "display world on console")
+)
 
 func rowLine() {
 	fmt.Print("\n")
-	for i := 0; i < maxCols; i++ {
+	for i := 0; i < *maxCols; i++ {
 		fmt.Print(" -----")
 	}
 	fmt.Print("\n")
 }
 
-func countLiveNeighbourCell(arr [maxRows][maxCols]int, curr_row int, curr_col int) int {
+func countLiveNeighbourCell(arr [][]int, curr_row int, curr_col int) int {
 	count := 0
 
 	for i := curr_row-1; i <= curr_row+1; i++ {
 		for j := curr_col-1; j <= curr_col+1; j++ {
-			if (i==curr_row && j==curr_col) || (i<0 || j<0) || (i>=maxRows || j>=maxCols) {
+			if (i==curr_row && j==curr_col) || (i<0 || j<0) || (i>=*maxRows || j>=*maxCols) {
 				continue
 			}
 			if arr[i][j]==1 {
@@ -33,33 +37,70 @@ func countLiveNeighbourCell(arr [maxRows][maxCols]int, curr_row int, curr_col in
 	return count
 }
 
-func nextWorldState(world01 *[maxRows][maxCols]int, world02 *[maxRows][maxCols]int) {
+func nextWorldState(world01 *[][]int, world02 *[][]int) {
 	neighbour_live_cell := 0
 	
-	for i := 0; i < maxRows; i++ {
-		for j := 0; j < maxCols; j++ {
+	for i := 0; i < *maxRows; i++ {
+		for j := 0; j < *maxCols; j++ {
 			neighbour_live_cell = countLiveNeighbourCell(*world01, i, j)
 
 			if (*world01)[i][j]==1 && (neighbour_live_cell==2 || neighbour_live_cell==3) {
-                world02[i][j] = 1
+                (*world02)[i][j] = 1
             } else if (*world01)[i][j]==0 && neighbour_live_cell==3 {
-                world02[i][j] = 1
+                (*world02)[i][j] = 1
             } else {
-                world02[i][j] = 0
+                (*world02)[i][j] = 0
             }
 		}
 	}
 }
 
-func main() {
-	world01 := [maxRows][maxCols]int{}
-	world02 := [maxRows][maxCols]int{}
+func printWorld(world *[][]int) {
+	rowLine()
+    for i := 0; i < *maxRows; i++ {
+        fmt.Print(":")
+        for j := 0; j < *maxCols; j++ {
+            fmt.Printf("  %d  :", (*world)[i][j])
+        }
+        rowLine()
+    }
+}
 
-	for i := 0; i < maxRows; i++ {
-		for j := 0; j < maxCols; j++ {
+
+func main() {
+	flag.Parse()
+
+	world01 := make([][]int, *maxRows)
+	for i := 0; i < *maxRows; i++ {
+		world01[i] = make([]int, *maxCols)
+	}
+
+	for i := 0; i < *maxRows; i++ {
+		for j := 0; j < *maxCols; j++ {
 			world01[i][j] = rand.Intn(32767) % 2
 		}
 	}
 
+	world02 := make([][]int, *maxRows)
+	for i := 0; i < *maxRows; i++ {
+		world02[i] = make([]int, *maxCols)
+	}
+
+	for i := 0; i < *maxRows; i++ {
+		for j := 0; j < *maxCols; j++ {
+			world02[i][j] = 0
+		}
+	}
+
+	if *displayWorld {
+		printWorld(&world01)
+	}
+	
+
 	nextWorldState(&world01, &world02)
+
+	if *displayWorld {
+		fmt.Println("Next Generation:")
+		printWorld(&world02)
+	}
 }
